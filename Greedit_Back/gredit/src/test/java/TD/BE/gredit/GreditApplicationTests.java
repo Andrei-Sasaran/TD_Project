b68bc43cd866ef51ae7ea57d0aa56cd16edd2c5c
Sasaran Andrei <sasaranandreimihnea@gmail.com>
2024-05-27 10:23:01 +0300

    initial back-end commit

---
+++ Greedit_Back/gredit/src/test/java/TD/BE/gredit/GreditApplicationTests.java
@@ -0,0 +1,13 @@
+package TD.BE.gredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
